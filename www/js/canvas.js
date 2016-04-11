9e26fe1ce1c1d4f0bc49cc87b5c8f8f52bd0538a
parisandmilo <di.klhy@gmail.com>
2016-04-11 22:42:15 +0100

    getting rid of unneccessary code...

--- www/js/canvas.js
+++ www/js/canvas.js
@@ -1,12 +1,43 @@
-var cvs, ctx;
-function init() {
-    var cvs = document.getElementById('myCanvas');
-    var ctx = cvs.getContext("2d");
-    var imageObj = new Image();
-    imageObj.onload = function() {
-    ctx.drawImage(imageObj, 5, 5);
-    };
-        // imageObj.src = 'img/Beach1A.jpg';
-};
+$(document).ready(
+    function() {
+    
+        var canvas = document.getElementById("imgCanvas");
+        var context = canvas.getContext("2d");
 
-window.onload = init;
+        function createImageOnCanvas(imageId) {
+            canvas.style.display = "block";
+            document.getElementById("images").style.overflowY = "hidden";
+            var img = new Image(300, 300);
+            img.src = document.getElementById(imageId).src;
+            context.drawImage(img, (0), (0)); //onload....
+        }
+
+        function draw(e) {
+            var pos = getMousePos(canvas, e);
+            posx = pos.x;
+            posy = pos.y;
+            context.strokeStyle = "#00FF00";
+            context.beginPath();
+            context.arc(posx, posy, 20, 0, 2*Math.PI);
+            context.stroke();
+        }
+
+        function getMousePos(canvas, evt) {
+            var rect = canvas.getBoundingClientRect();
+            return {
+              x: evt.clientX - rect.left,
+              y: evt.clientY - rect.top
+            };
+        }
+        window.draw = draw;
+
+
+        // var cvs, ctx;
+        // cvs = document.getElementById('myCanvas');
+        // ctx = cvs.getContext("2d");
+        // var imageObj = new Image();
+        // imageObj.onload = function() {
+        // ctx.drawImage(imageObj, 5, 5);
+        // };
+            // imageObj.src = 'img/Beach1A.jpg';
+});
--- www/js/main.js
+++ www/js/main.js
@@ -14,36 +14,6 @@ function createCanvas() {
     ctx.drawImage(img,100,100);
 };
 
-var canvas = document.getElementById("imgCanvas");
-var context = canvas.getContext("2d");
-
-function createImageOnCanvas(imageId) {
-    canvas.style.display = "block";
-    document.getElementById("images").style.overflowY = "hidden";
-    var img = new Image(300, 300);
-    img.src = document.getElementById(imageId).src;
-    context.drawImage(img, (0), (0)); //onload....
-}
-
-function draw(e) {
-    var pos = getMousePos(canvas, e);
-    posx = pos.x;
-    posy = pos.y;
-    context.strokeStyle = "#00FF00";
-    context.beginPath();
-    context.arc(posx, posy, 20, 0, 2*Math.PI);
-    context.stroke();
-}
-
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-}
-window.draw = draw;
-
 function addToDo(subjectCode,age,gender,record,time) {
   var todo = {
     _id: new Date().toISOString(),
