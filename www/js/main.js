599ddc2ae94fe82442688596f8e6160a42c030c4
tudornica <tudornica95@gmail.com>
2016-04-21 00:04:37 +0100

    correct differences spotted

--- www/js/main.js
+++ www/js/main.js
@@ -4,24 +4,22 @@ function updateWindow(prev, next){
     $("#" + next).show();
   }
 }
+
 function correctData(screen, next){
   if(screen === "secondScreen" && next === "thirdScreen"){
-    if(document.getElementById("subjCode").value === "")
-    {
+    if(document.getElementById("subjCode").value === ""){
       document.getElementById('err').innerHTML = "Please insert your subject code";
       showAlertPopup();
       return false;
     }
-    if(document.getElementById("gender").value === "0")
-    {
+    if(document.getElementById("gender").value === "0"){
       document.getElementById('err').innerHTML = "Please select your gender";
       showAlertPopup();
       return false;
     }
   }
   if(screen === "thirdScreen" && next === "fourthScreen"){
-    if(document.getElementById("picture").value === "empty")
-    {
+    if(document.getElementById("picture").value === "empty"){
       document.getElementById('err').innerHTML = "Please select a picture";
       showAlertPopup();
       return false;
@@ -47,9 +45,32 @@ function showConfirmPopup(){
   $("#" + "confirmButtons").show();
 }
 
+function confirmFinish() {
+  popupCloseEvent = -2;
+  if(!timeIsUp){
+    document.getElementById('err').innerHTML = "Are you sure you want to finish the game?\n You won't be able to continue after you finish.";
+    showConfirmPopup();
+  }
+  else
+    confirmPopup();
+}
+
 function confirmPopup(){
-  if(popupCloseEvent === -1)
+  if(popupCloseEvent == -2)
   {
+    finalCheck();
+    pauseTimer();
+    $("#" + "timerDiv").hide();
+    $("#" + "resultDiv").show();
+    $("#" + "finishButton").hide();
+    $("#" + "submitButton").show();
+  }
+  if(popupCloseEvent == -1)
+  {
+    $("#" + "timerDiv").show();
+    $("#" + "resultDiv").hide();
+    $("#" + "finishButton").show();
+    $("#" + "submitButton").hide();
     updateWindow("fifthSCreen", "fourthScreen");
     pauseTimer();
   }
@@ -82,7 +103,8 @@ function showPause(){
   document.getElementById("img").classList.remove("blur-filter");
   document.getElementById("imgCanvas").classList.remove("blur-filter");
 }
-function updateTextInput(val) {
+
+function updateSlideText(val) {
   if(val == 1)
   	document.getElementById('timevalue').innerHTML = val + " minute ";
   else
@@ -134,7 +156,7 @@ function handleEvent(e) {
       timeLog[totalTaps].difference = foundDiff[i];
       confirmRemoval(i);
       draw();
-      console.log(timeLog[totalTaps]);
+      //console.log(timeLog[totalTaps]);
       return;
     }
 
@@ -144,7 +166,7 @@ function handleEvent(e) {
     timeLog[totalTaps].action = "exceeded 12 differences";
     timeLog[totalTaps].difference = new Object;
     timeLog[totalTaps].difference.pos = pos;
-    console.log(timeLog[totalTaps]);
+    //console.log(timeLog[totalTaps]);
     return;
   }
 
@@ -154,7 +176,7 @@ function handleEvent(e) {
   timeLog[totalTaps].action = "difference spotted";
   timeLog[totalTaps].difference = foundDiff[numDiff];
 
-  console.log(timeLog[totalTaps]);
+  //console.log(timeLog[totalTaps]);
 }
 
 function getMousePos(canvas, evt) {
@@ -206,10 +228,10 @@ function draw(){
   var context = canvas.getContext("2d");
 
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.lineWidth = 7;
+
   for(i=1; i<=numDiff; i++){
     if(i == popupCloseEvent)
-      context.strokeStyle = "#FF8000"
+      context.strokeStyle = "#FF8000";
     else
       context.strokeStyle = "#00FF00";
     context.beginPath();
@@ -255,7 +277,6 @@ function recordAudio() {
     // Record audio
     mediaRec.startRecord();
 }
-// Timer
 
 var time;
 var seconds, minutes;
@@ -276,6 +297,7 @@ function startTimer(){
   document.getElementById('timer').innerHTML += minutes.toString() + ":00";
 
   paused = false;
+  timeIsUp = false;
   time = setInterval(updateTimer, 1000);
 }
 
@@ -290,14 +312,19 @@ function resumeTimer() {
   time = setInterval(updateTimer, 1000);
 }
 
+var timeIsUp;
+
 function updateTimer(){
     if(document.getElementById('timer').innerHTML === "00:00"){
+      timeIsUp = true;
     	paused = true;
-        clearInterval(time);
-        document.getElementById('err').innerHTML = "Time is up!";
-        showAlertPopup();
-        //alert("Time is up!");
-        return;
+      clearInterval(time);
+      popupCloseEvent = 0;
+      closeModal();
+      draw();
+      document.getElementById('err').innerHTML = "Time is up!";
+      showAlertPopup();
+      return;
     }
 
     seconds--;
@@ -313,27 +340,13 @@ function updateTimer(){
     if(seconds < 10)
         document.getElementById('timer').innerHTML += "0"
     document.getElementById('timer').innerHTML += seconds.toString();
-
 }
 
-
-
-
-
-
-
-// Get the <span> element that closes the modal
-
-// When the user clicks the button, open the modal 
-// When the user clicks on <span> (x), close the modal
-function closeModal() {
-  
-  document.getElementById('popup').style.display = "none";
+function closeModal() { 
   $("#" + "popup").hide();
   $("#" + "popup2").hide();
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   var popup = document.getElementById('popup');
   if (event.target == popup) {
@@ -347,4 +360,83 @@ window.onclick = function(event) {
       draw();
       return;
   }
+}
+
+
+function finalCheck(){
+  var correctDiffs = [], i, j;
+  
+  correctDiffs["img/beach1"] = [{x: -1, y: -1}, {x: 0.926, y: 0.639}, {x: 0.925, y: 0.420}, {x: 0.795, y: 0.414}, {x: 0.698, y: 0.328}, {x: 0.563, y: 0.593}, {x: 0.583, y: 0.868}, {x: 0.101, y: 0.846}, {x: 0.113, y: 0.498}, {x: 0.049, y: 0.358}, {x: 0.126, y: 0.132}, {x: 0.223, y: 0.208}, {x: 0.476, y: 0.437}];
+  correctDiffs["img/beach2"] = [{x: -1, y: -1}, {x: 0.908, y: 0.433}, {x: 0.809, y: 0.193}, {x: 0.936, y: 0.214}, {x: 0.750, y: 0.171}, {x: 0.893, y: 0.658}, {x: 0.389, y: 0.095}, {x: 0.136, y: 0.182}, {x: 0.065, y: 0.214}, {x: 0.180, y: 0.353}, {x: 0.047, y: 0.823}, {x: 0.417, y: 0.710}, {x: 0.910, y: 0.862}]
+  correctDiffs["img/beach4"] = [{x: -1, y: -1}, {x: 0.969, y: 0.132}, {x: 0.839, y: 0.201}, {x: 0.572, y: 0.127}, {x: 0.714, y: 0.338}, {x: 0.887, y: 0.642}, {x: 0.806, y: 0.805}, {x: 0.646, y: 0.635}, {x: 0.330, y: 0.249}, {x: 0.217, y: 0.464}, {x: 0.363, y: 0.553}, {x: 0.269, y: 0.713}, {x: 0.147, y: 0.803}]
+  correctDiffs["img/farm1"] = [{x: -1, y: -1}, {x: 0.643, y: 0.323}, {x: 0.624, y: 0.429}, {x: 0.969, y: 0.517}, {x: 0.715, y: 0.658}, {x: 0.943, y: 0.710}, {x: 0.232, y: 0.635}, {x: 0.153, y: 0.369}, {x: 0.056, y: 0.260}, {x: 0.205, y: 0.239}, {x: 0.250, y: 0.922}, {x: 0.266, y: 0.510}, {x: 0.723, y: 0.861}]
+  correctDiffs["img/farm2"] = [{x: -1, y: -1}, {x: 0.887, y: 0.360}, {x: 0.627, y: 0.274}, {x: 0.803, y: 0.464}, {x: 0.193, y: 0.125}, {x: 0.426, y: 0.412}, {x: 0.901, y: 0.735}, {x: 0.829, y: 0.740}, {x: 0.741, y: 0.903}, {x: 0.330, y: 0.745}, {x: 0.211, y: 0.849}, {x: 0.146, y: 0.596}, {x: 0.455, y: 0.165}]
+  correctDiffs["img/farm4"] = [{x: -1, y: -1}, {x: 0.968, y: 0.147}, {x: 0.847, y: 0.312}, {x: 0.715, y: 0.258}, {x: 0.962, y: 0.593}, {x: 0.685, y: 0.555}, {x: 0.941, y: 0.856}, {x: 0.421, y: 0.861}, {x: 0.243, y: 0.295}, {x: 0.472, y: 0.277}, {x: 0.042, y: 0.811}, {x: 0.339, y: 0.404}, {x: 0.416, y: 0.553}]
+  correctDiffs["img/street2"] = [{x: -1, y: -1}, {x: 0.859, y: 0.338}, {x: 0.937, y: 0.428}, {x: 0.892, y: 0.553}, {x: 0.678, y: 0.057}, {x: 0.595, y: 0.249}, {x: 0.465, y: 0.135}, {x: 0.524, y: 0.840}, {x: 0.310, y: 0.685}, {x: 0.114, y: 0.941}, {x: 0.104, y: 0.246}, {x: 0.249, y: 0.086}, {x: 0.575, y: 0.059}]
+  correctDiffs["img/street3"] = [{x: -1, y: -1}, {x: 0.965, y: 0.334}, {x: 0.931, y: 0.645}, {x: 0.616, y: 0.945}, {x: 0.735, y: 0.418}, {x: 0.670, y: 0.200}, {x: 0.852, y: 0.428}, {x: 0.353, y: 0.685}, {x: 0.400, y: 0.393}, {x: 0.230, y: 0.358}, {x: 0.205, y: 0.190}, {x: 0.220, y: 0.658}, {x: 0.061, y: 0.688}]
+
+
+  var canvas = document.getElementById("imgCanvas");
+  var context = canvas.getContext("2d");
+  var img = document.getElementById("picture").value;
+  var ok;
+  var spottedDiffs = 0;
+
+  for(i=1;i<=12;i++){
+    correctDiffs[img][i].x = correctDiffs[img][i].x*canvas.width;
+    correctDiffs[img][i].y *= canvas.height;
+  }
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.strokeStyle = "#00FF00";
+
+  for(i=1; i<=numDiff; i++){
+    ok = false;
+    distmin = 999999;
+    for(j=1;j<=12;j++)
+      if(dist(foundDiff[i].pos, correctDiffs[img][j]) <= circleRadius + 10)
+      {
+        spottedDiffs++;
+        ok = true;
+        correctDiffs[img][j].x = -10000;
+        break;
+      }
+    /*if(ok)
+      context.strokeStyle = "#22B14C";*/
+    context.strokeStyle = "#00FF00";
+    if(ok)
+      context.strokeStyle = "#046402";
+    context.beginPath();
+    context.arc(foundDiff[i].pos.x, foundDiff[i].pos.y, circleRadius, 0, 2*Math.PI);
+    context.lineWidth = 4;
+    context.stroke();
+    if(!ok){
+      context.strokeStyle = "#ED1C24";
+      context.beginPath();
+      context.moveTo(foundDiff[i].pos.x - circleRadius, foundDiff[i].pos.y - circleRadius);
+      context.lineTo(foundDiff[i].pos.x + circleRadius, foundDiff[i].pos.y + circleRadius);
+      context.lineWidth = 2;
+      context.stroke();
+      context.moveTo(foundDiff[i].pos.x + circleRadius, foundDiff[i].pos.y - circleRadius);
+      context.lineTo(foundDiff[i].pos.x - circleRadius, foundDiff[i].pos.y + circleRadius);
+      context.lineWidth = 2;
+      context.stroke();
+    }
+  }
+
+  context.strokeStyle = "#F24044";
+
+  for(i=1; i<=12; i++){
+    if(correctDiffs[img][i].x == -10000)
+      continue;
+    context.beginPath();
+    context.arc(correctDiffs[img][i].x, correctDiffs[img][i].y, circleRadius, 0, 2*Math.PI);
+    context.lineWidth = 4;
+    context.stroke();
+  }
+
+  if(spottedDiffs == 1)
+    document.getElementById("result").innerHTML = "You got " + spottedDiffs.toString() + " difference right!";
+  else
+    document.getElementById("result").innerHTML = "You got " + spottedDiffs.toString() + " differences right!";
 }
