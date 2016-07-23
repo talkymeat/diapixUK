e21e475900360d07292f9eda7e48946575db2b7c
Diana Lee <diana@awebots.com>
2016-07-23 04:12:43 +0100

    fix app to work with API

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+var app = express();
+var http = require('http');
+const socketIO = require('socket.io');
+
+
+app.use(express.static('www'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    next()
+});
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
