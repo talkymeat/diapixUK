0762b9bf25336038004cdbad47248c2d52a877a9
Diana Lee <diana@awebots.com>
2016-07-23 05:30:33 +0100

    fixing socket connection for android

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.all('*', function(req, res, next) {
     next()
 });
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 80);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
